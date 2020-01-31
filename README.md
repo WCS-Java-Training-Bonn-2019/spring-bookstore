915dba22482e9e9500ec9d3d09a9cf3f48673de7
Markus Schneider <markus@beefcafe.de>
2020-01-31 11:16:35 +0100

    Added initial dataloading for test environment

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # spring-bookstore
+
+## Create database
+
+```
+CREATE DATABASE IF NOT EXISTS `book_shop_jpa`;
+CREATE USER IF NOT EXISTS 'h4rryp0tt3r'@'localhost' IDENTIFIED BY 'Horcrux4life!';
+GRANT ALL PRIVILEGES ON book_shop_jpa.* TO 'h4rryp0tt3r'@'localhost';
+```
+
+### Creating updated schema.sql / data.sql files
+
+```
+mysqldump -u root -p --no-data book_shop_jpa > src/main/resources/schema.sql
+mysqldump -u root -p --no-create-info book_shop_jpa > src/main/resources/data.sql
+
+sed -i '1iSET FOREIGN_KEY_CHECKS=0;' src/main/resources/schema.sql
+echo "SET FOREIGN_KEY_CHECKS=1;" >> src/main/resources/schema.sql
+sed -i '1iSET FOREIGN_KEY_CHECKS=0;' src/main/resources/data.sql
+echo "SET FOREIGN_KEY_CHECKS=1;" >> src/main/resources/data.sql
+```
---
+++ src/main/resources/data.sql
@@ -0,0 +1,99 @@
+SET FOREIGN_KEY_CHECKS=0;
+-- MySQL dump 10.13  Distrib 5.7.29, for Linux (x86_64)
+--
+-- Host: localhost    Database: book_shop_jpa
+-- ------------------------------------------------------
+-- Server version	5.7.29-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `author`
+--
+
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+INSERT INTO `author` VALUES (1,'Robert C.','Martin'),(2,'Harry','Petersen');
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (2,99,'978-1-78281-781-9','Commodore: The Inside Story',NULL),(3,20,'978-1-61729-356-6','Modern Java In Action',1),(4,7,'978-1-93435-659-3','Seven Languages in Seven Weeks',NULL),(5,10,'978-1-4302-1833-3','Pro Git',NULL);
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `book_author`
+--
+
+LOCK TABLES `book_author` WRITE;
+/*!40000 ALTER TABLE `book_author` DISABLE KEYS */;
+INSERT INTO `book_author` VALUES (5,1),(5,2),(2,1),(2,2);
+/*!40000 ALTER TABLE `book_author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (3,'GEHEIM','Markus','Schneider'),(4,'sadsadasd','Martin','Schmitz');
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `customer_order`
+--
+
+LOCK TABLES `customer_order` WRITE;
+/*!40000 ALTER TABLE `customer_order` DISABLE KEYS */;
+INSERT INTO `customer_order` VALUES (2,1,2,3);
+/*!40000 ALTER TABLE `customer_order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `publisher`
+--
+
+LOCK TABLES `publisher` WRITE;
+/*!40000 ALTER TABLE `publisher` DISABLE KEYS */;
+INSERT INTO `publisher` VALUES (1,'Prentice Hall'),(4,'1');
+/*!40000 ALTER TABLE `publisher` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `review`
+--
+
+LOCK TABLES `review` WRITE;
+/*!40000 ALTER TABLE `review` DISABLE KEYS */;
+INSERT INTO `review` VALUES (5,'Soso',2,3),(9,'asdasd',2,4),(5,'Soso',3,3);
+/*!40000 ALTER TABLE `review` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-31 11:15:10
+SET FOREIGN_KEY_CHECKS=1;
